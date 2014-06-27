453811adeb1573ac7339d7cf4ce9a97a96df96db
Gabriel Naiman <gnaiman@keepcon.com>
2014-06-27 18:35:20 -0300

    Added Puma and Rack::Static

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'puma'
 gem 'cuba'
 gem 'tilt'
 gem 'slim'
--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'cuba'
 require 'cuba/render'
 require 'yaml'
 
+CONFIG = YAML.load_file(File.expand_path('../config.yml', __FILE__))
+
 Cuba.plugin Cuba::Render
 Cuba.settings[:render][:template_engine] = :slim
 
@@ -11,19 +13,20 @@ module Helper
     (mb / 1024).round(2)
   end
 end
-
-config = YAML.load_file(File.expand_path('../config.yml', __FILE__))
-
 Cuba.plugin Helper
+
+Cuba.use Rack::Static,
+         urls: %w[/fonts /images /js /css /bower_components],
+         root: File.expand_path('public', __dir__)
 
 Cuba.define do
 
   on get, root do
-    res.write view(:dashboard, machines: config['servers'].keys)
+    res.write view(:dashboard, machines: CONFIG['servers'].keys)
   end
 
   on get, 'machine/:name' do |name|
-    server = config['servers'][name]
+    server = CONFIG['servers'][name]
     Datacenter::Shell::Ssh.open(server['host'], server['user'], port: server['port'] || 22) do |shell|
       machine = Datacenter::Machine.new shell
       res.write partial(:machine, machine: machine)
