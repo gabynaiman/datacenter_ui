e1fc683ae6b4d1e0cf69dacc0c9bd6279b6f6c47
Gabriel Naiman <gnaiman@keepcon.com>
2014-06-27 18:29:54 -0300

    Ajax version

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'datacenter'
 require 'cuba'
 require 'cuba/render'
+require 'yaml'
 
 Cuba.plugin Cuba::Render
 Cuba.settings[:render][:template_engine] = :slim
@@ -11,20 +12,21 @@ module Helper
   end
 end
 
+config = YAML.load_file(File.expand_path('../config.yml', __FILE__))
+
 Cuba.plugin Helper
 
 Cuba.define do
 
   on get, root do
-    machines = File.read(File.expand_path('~/.ssh/config')).scan(/Host (.*)\n/).flatten.sort
-    res.write view(:dashboard, machines: machines)
+    res.write view(:dashboard, machines: config['servers'].keys)
   end
 
   on get, 'machine/:name' do |name|
-    config = Net::SSH::Config.for name
-    Datacenter::Shell::Ssh.open(config[:host_name], config[:user]) do |shell|
+    server = config['servers'][name]
+    Datacenter::Shell::Ssh.open(server['host'], server['user'], port: server['port'] || 22) do |shell|
       machine = Datacenter::Machine.new shell
-      res.write view(:machine, machine: machine)
+      res.write partial(:machine, machine: machine)
     end
   end
 
